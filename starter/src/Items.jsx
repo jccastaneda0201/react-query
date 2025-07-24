dc55b03afade92de401cd43e4d2739556b957365
Juan Castaneda <jccastanedacamargo@outlook.com>
2025-07-24 16:43:39 +0200

    feat: add initial project files

---
+++ starter/src/Items.jsx
@@ -0,0 +1,30 @@
+import { useQuery } from '@tanstack/react-query';
+import SingleItem from './SingleItem';
+import customFetch from './utils';
+
+const Items = () => {
+  const { isLoading, data, isError, error } = useQuery({
+    queryKey: ['tasks'],
+    queryFn: async () => {
+      const { data } = await customFetch.get('/something');
+      return data;
+    },
+  });
+
+  if (isLoading) {
+    return <p style={{ marginTop: '2rem' }}>Loading...</p>;
+  }
+
+  if (isError) {
+    return <p style={{ marginTop: '2rem' }}>{error.response.data}</p>;
+  }
+
+  return (
+    <div className="items">
+      {data.data.taskList.map((item) => {
+        return <SingleItem key={item.id} item={item} />;
+      })}
+    </div>
+  );
+};
+export default Items;
